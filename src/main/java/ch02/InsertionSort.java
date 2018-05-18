c9b226e737e327020aa642daeb0f5638a9e7abff
nathanjekel <ijekel2@gmail.com>
2018-05-18 10:44:30 -0400

    ch02 - insertion sort

---
+++ src/main/java/ch02/InsertionSort.java
@@ -0,0 +1,26 @@
+package main.java.ch02;
+
+/**
+ * Created by nathanjekel on 5/18/18.
+ */
+public class InsertionSort {
+
+    public static int[] Sort(int[] array) {
+
+        for(int i = 1; i < array.length; i++){
+
+            int key = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && array[j] > key){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+
+        }
+
+        return array;
+
+    }
+}
---
+++ src/test/java/ch02/InsertionSortTest.java
@@ -0,0 +1,21 @@
+package test.java.ch02;
+
+import main.java.ch02.InsertionSort;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by nathanjekel on 5/18/18.
+ */
+public class InsertionSortTest {
+
+    @Test
+    public void testSortWithWorstCase(){
+        int[] unsortedArray = {4, 3, 2, 1, 0};
+        int[] expectedArray = {0, 1, 2, 3, 4};
+        int[] sortedArray = InsertionSort.Sort(unsortedArray);
+
+        assertArrayEquals(expectedArray, sortedArray);
+
+    }
+}
